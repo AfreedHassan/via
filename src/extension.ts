b6c5fbcfc571c059a2d383d73e8c24f38854685e
AfreedHassan <afreed.hassan16@gmail.com>
2025-09-13 04:30:15 -0400

    feat: add extension entry point
    - Register HelloWorldProvider for webview
    - Setup basic extension activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,16 @@
 import * as vscode from 'vscode';
+import { HelloWorldProvider } from './helloWorldProvider';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "via" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('via.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from Via!');
-    });
-
-    context.subscriptions.push(disposable);
+    // Register our custom webview provider
+    const helloWorldProvider = new HelloWorldProvider(context.extensionUri);
+    
+    context.subscriptions.push(
+        vscode.window.registerWebviewViewProvider(
+            HelloWorldProvider.viewType,
+            helloWorldProvider
+        )
+    );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
